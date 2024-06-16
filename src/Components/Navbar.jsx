fe73df5765143063d27b4562e729cc7d8072d1f9
kponky <ikponkedet@gmail.com>
2024-06-16 18:23:59 +0100

    navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,24 +2,30 @@ import React from "react";
 import logo from "../assets/Unijos Logo.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import '../Styles/navbar.css'
 // import { HashLink as Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="nav-logo">
-          <img src={logo} alt="unijos logo" />
-          <h3>Uinversity of Jos</h3>
+        <div className="navbar-logo">
+          <img src={logo} alt="unijos logo" className="navlogo" />
+          <h3>University of Jos</h3>
         </div>
 
         <ul className="nav-links">
           <li>Home</li>
+          <li>About</li>
           <li>Departments</li>
           <li>Login</li>
-         <button> <FontAwesomeIcon icon={faSearch}/> </button>
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
         </ul>
       </div>
+
+      
     </nav>
   );
 };
